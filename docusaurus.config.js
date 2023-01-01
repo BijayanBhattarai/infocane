37efa733679feda9a081da693d79287c43303532
Bijayandai <bjnboysm@gmail.com>
2023-01-01 22:31:39 +0545

    cleaning welcome page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'InfoCane',
+  tagline: 'Source of tasty inforamtion',
   url: 'https://github.com',
   baseUrl: '/infocane/',
   onBrokenLinks: 'throw',
@@ -18,6 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'BijayanBhattarai', // Usually your GitHub org/user name.
   projectName: 'infocane', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
